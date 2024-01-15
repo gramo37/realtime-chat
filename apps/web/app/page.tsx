626545d5a2361fa735b0501813bbf303dbed0fff
LAPTOP29\Prasanna <prasanna.gramopadhye@optanium.com>
2024-01-15 15:55:41 +0530

    Added Login and dashboard page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,24 @@
-export default function Page() {
+import { cookies } from "next/headers";
+
+import { Mail } from "@/components/mail";
+import { accounts, mails } from "@/app/data";
+
+export default function MailPage() {
+  const layout = cookies().get("react-resizable-panels:layout");
+  const collapsed = cookies().get("react-resizable-panels:collapsed");
+
+  const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+  const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
+
   return (
-    <>
-      <h1>Web</h1>
-    </>
+    <div className="hidden flex-col md:flex">
+      <Mail
+        accounts={accounts}
+        mails={mails}
+        defaultLayout={defaultLayout}
+        defaultCollapsed={defaultCollapsed}
+        navCollapsedSize={4}
+      />
+    </div>
   );
 }
